a19ee4c95b0c40e31b3c5fcc24b8ec3273083fc5
PranaliAdsul <pranuadsulst1137@gmail.com>
2024-04-18 15:57:29 +0530

    Add to cart and BuyNow button added

--- src/Component/ArtWork/Artworks.js
+++ src/Component/ArtWork/Artworks.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { Card, Avatar, Row, Col } from 'antd';
+import { Card, Avatar, Row, Col, Button } from 'antd';
+import { ShoppingCartOutlined, ShoppingOutlined } from '@ant-design/icons'; // Import icons
 
 const Artworks = () => {
   const [artworks, setArtworks] = useState([]);
@@ -23,6 +24,14 @@ const Artworks = () => {
             <Card
               hoverable
               cover={<img alt={artwork.name} src={artwork.art} />}
+              actions={[
+                <div>
+                  <Button icon={<ShoppingCartOutlined />} key="addToCart">Add</Button>
+                </div>,
+                <div>
+                  <Button icon={<ShoppingOutlined />} key="buyNow">Buy Now</Button>
+                </div>
+              ]}
             >
               <Card.Meta
                 avatar={<Avatar src={artwork.avatar} />}
